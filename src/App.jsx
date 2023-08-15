93c04518abe8f5ea9728ce5146964301e5b6f76c
Agailh <agailham86@gmail.com>
2023-08-15 10:08:18 +0700

    new

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+import Navbar from './Components/Navbar'
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar />
+      
+    </>
+  )
+}
+
+export default App
